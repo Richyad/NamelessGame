05b46d931fa6516027454c74663a7b4d7cae00f9
Richyad <lclarkerichard@yahoo.com>
2017-01-26 15:34:02 -0500

    Update nameless.java

--- nameless.java
+++ nameless.java
@@ -8,9 +8,8 @@ At any time during the program when the user enters inaccurate input:
 If incorrect choice is selected, display twice before exiting
 2. Display Stage 1: Awakening and prompt user to select a choice
 (1) Pray 
-You sit up and kneel facing your window as you pray for positivity and peace for the day. Should've prayed for a strategy guide to this game instead...
 (2) Poop
-You slide out of bed and saunter to the porcelain throne where you make your daily deposit to the doo doo demons. Unforunately, your body is still waking up so it's gonna be a while. But we have places to be and things to see! You decide to...
+
 Push 
 If only you waited a little longer! The Ben & Jerry's you had before bed propels the giant stool right to the exit of your booty, scraping your rectum on the way out. A blood coated turd slides out painfully. The doo doo demons, pleased with your blood oath, vow to watch over you today. But you must accept their guidance!
 Accept 
@@ -51,6 +50,13 @@ Each option = certain amt of time. Store as a running total and use arithmetic o
 Randomize the amt of time certain tasks take in order to keep the game fresh each time and add replay value.
 Display Stage 2: The Journey
 
+INTRO: To play this game, a girl must become...nameless?
+Include Catherine do you like cats or a dogs with alternate secret they both suck.
+
+
+
+
+Sent from my ASUS
 The laceration from the earlier dump starts to irritate you and you hop out of the seat by accident! You foot slides off the clutch and you jerk the wheel to the right sending you careening across the highway just barely swerving across four lanes.
 You pull to the side of the road scratching your head (and butt) wondering how the heck you didn't just crash. This cut is becoming a real bother. You decide to
 Go wipe up in the woods
@@ -85,52 +91,82 @@ public class nameless{
 
 			if(input.equals("Y") || input.equals("y"))
 				System.out.println("Okay!");
-			else if(input.equals("N") || input.equals("n"))
+			else if(input.equals("N") || input.equals("n")){
 				System.out.println("Okay. Well, learn your name and run the program again. BYE");
+				System.exit(1);
+			}
 			else{
-				System.out.println("I didn’t catch that.");
+				System.out.println("I didn\'t catch that.");
 				System.out.print(name + ", huh? Y/N\t\t");
 				input = read.nextLine();
+					if(input.equals("Y") || input.equals("y"))
+						System.out.println("Okay!");
 			}
 			//Second Check
-			if(input.equals("Y") || input.equals("y"))
-				System.out.println("Okay!");
-			else if(input.equals("N") || input.equals("n"))
+			if(input.equals("N") || input.equals("n")){
 				System.out.println("Okay. Well, learn your name and run the program again. BYE");
-			else{
-				System.out.println("I STILL didn’t catch that.");
+				System.exit(1);
+			}
+			else if (!(input.equals("Y") || input.equals("y"))){
+				System.out.println("I STILL didn\'t catch that.");
 				System.out.print(name + ", huh? Y/N\t\t");
 				input = read.nextLine();
+					if(input.equals("Y") || input.equals("y"))
+						System.out.println("Okay!");
 			}
 			//Final Check
-			if(input.equals("Y") || input.equals("y"))
-				System.out.println("Okay!");
-			else 
+			if(input.equals("N") || input.equals("n")){
 				System.out.println("Okay. Well, learn your name and run the program again. BYE");
+				System.exit(1);
+			}
+			else if(!(input.equals("Y") || input.equals("y"))){
+				System.out.println("Only Y/N are allowed! Run the program again. BYE");
+				System.exit(1);
+			}
 
 		//Display Stage 1: Awakening and prompt the user to select a choice
 		System.out.println("You awaken in your bed. It's Monday. The time is 6am and you have to be at work at 9." +
 						   " What's the move?");
-		System.out.println("(1) Pray\t(2) Poop\t(3) Play game"); 
+		System.out.print("(1) Pray\t(2) Poop\t(3) Play game\t"); 
 		input = read.nextLine();
+			//(1)Pray
 			if(input.equals("1")){
-				System.out.println("You sit up and kneel facing your window as you pray for positivity and peace " +
-								   "for the day. Should've prayed for a strategy guide to this game instead...");
+				System.out.println("You sit up and kneel facing your window as you pray for positivity and peace" +
+								   "\nfor the day. Should've prayed for a strategy guide to this game instead...");
+				//pause HERE
 			}
-		
+			//(2)Poop
 			else if(input.equals("2")){
-				System.out.println("You slide out of bed and saunter to the porcelain throne where you make " +
-								   "your daily deposit to the doo doo demons.\nUnforunately, your body is still " +
+				System.out.print("You slide out of bed and saunter to the porcelain throne where you make " +
+								   "your daily deposit to the doo doo demons." +
+								   "\nUnforunately, your body is still " +
 								   "waking up so it's gonna be a while.\nINSERT A PAUSE HERE But we have places " +
-								   "to be and things to see! You decide to...");
+								   "to be and things to see!\nYou decide to...");
+				System.out.println("(P) Push\t(W) Wait\t");
+				input = read.nextLine();
+					//(2a)Push
+					if(input.equals("P") || input.equals("p")){
+						System.out.println("If only you waited a little longer! The Ben & Jerry's you had "+
+										   "before bed propels the giant stool right to the exit of your " +
+										   "booty, scraping your rectum on the way out.\nA blood coated " +
+										   "turd slides out painfully.\nThe doo doo demons, pleased with " +
+										   "your blood oath, vow to watch over you on your journey today.\nBut, you must " +
+										   "accept their guidance!");
+						System.out.println("(A) Accept doo doo demon guidance\t(D) Decline this foolish offer");
+						
+						//(2a.I) Accept
+						input = read.nextLine();
+							if((input.equals("A") || input.equals("a")))
+								System.out.println("This is the shit you asked for");
+						//(2a.II) Decline
+							else if((input.equals("D") || input.equals("d")))
+								System.out.println("Good job. No such thing as doo doo demons.\n" +
+												   "But you've unlocked a bonus. Just finish the game and claim your prize!");
+					}
+				
+
 			}
 /*
-Push 
-If only you waited a little longer! The Ben & Jerry's you had before bed propels the giant stool right to the exit of your booty, scraping your rectum on the way out. A blood coated turd slides out painfully. The doo doo demons, pleased with your blood oath, vow to watch over you today. But you must accept their guidance!
-Accept 
-This is the shit you asked for
-Decline
-Good job. No such thing as doo doo demons. But you've unlocked a bonus. Just finish the game and claim your prize!
 Wait
 It takes a while. In the meantime, you jump on twitter to look for saggy breast photos. An article appears on your feed about the Dangers of DooDoo Demons. As the poop approaches its birth you…
 Hold it
